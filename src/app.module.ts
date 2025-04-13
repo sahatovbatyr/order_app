56528c24d45d9ac8120df8e3b6d0d96eb00ddc08
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-14 01:32:54 +0300

    fixed: added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './configs/envValidationSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
