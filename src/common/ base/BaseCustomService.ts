1f7f8ffe8101a323f82bfb42fc38f9eb25ea372d
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-14 01:26:03 +0300

    fixed: added Relations

--- src/common/ base/BaseCustomService.ts
+++ src/common/ base/BaseCustomService.ts
@@ -1,9 +1,17 @@
-import { Repository, FindOptionsWhere } from 'typeorm';
+import { Repository, FindOptionsWhere, DeepPartial } from 'typeorm';
 import { BaseCustomEntity } from './BaseCustomEntity';
-import { NotFoundException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
 export abstract class BaseCustomService<T extends BaseCustomEntity> {
-  constructor(protected readonly repository: Repository<T>) {}
+  constructor(
+    protected readonly repository: Repository<T>,
+    private readonly entityName: string,
+  ) {}
+
+  protected abstract getRelations(): string[];
 
   async findById(id: number): Promise<T | null> {
     return this.repository.findOneBy({ id } as FindOptionsWhere<T>);
@@ -17,7 +25,32 @@ export abstract class BaseCustomService<T extends BaseCustomEntity> {
     return entity;
   }
 
-  async create(entity: Partial<T>): Promise<T> {
+  public async findOneByProp(
+    propName: keyof T,
+    propValue: any,
+  ): Promise<T | null> {
+    return await this.repository.findOne({
+      where: { [propName]: propValue },
+      relations: this.getRelations(),
+    } as any);
+  }
+
+  public async findOneByProp_orThrow(
+    propName: keyof T,
+    propValue: any,
+  ): Promise<T> {
+    const res = await this.findOneByProp(propName, propValue);
+
+    if (!res) {
+      throw new NotFoundException(
+        `Entity [${this.entityName}] not found ( ${String(propName)}: ${propValue})`,
+      );
+    }
+
+    return res;
+  }
+
+  async create(entity: DeepPartial<T>): Promise<T> {
     const newEntity = this.repository.create(entity);
     return this.repository.save(newEntity);
   }
@@ -35,5 +68,13 @@ export abstract class BaseCustomService<T extends BaseCustomEntity> {
 
   async find(): Promise<T[]> {
     return this.repository.find();
+  }
+
+  protected async save(entity: DeepPartial<T>) {
+    try {
+      return await this.repository.save(entity);
+    } catch (err: any) {
+      throw new InternalServerErrorException(`Error in server. ${err.message}`);
+    }
   }
 }
