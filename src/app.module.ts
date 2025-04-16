19b998a396195f2eafe6735d86208080d3531d20
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-16 21:14:32 +0300

    added: database module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import * as process from 'node:process';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './configs/envValidationSchema';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { envValidationSchema } from './configs/envValidationSchema';
       validationSchema: envValidationSchema,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
