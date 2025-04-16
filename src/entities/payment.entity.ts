bfdaaf355df0cfe243d354ee701ead0597a28522
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-17 02:01:51 +0300

    fixed:  Order and Payment classes

--- src/entities/payment.entity.ts
+++ src/entities/payment.entity.ts
@@ -1,3 +1,16 @@
 import { BaseCustomEntity } from '../common/ base/BaseCustomEntity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { User } from './user.entity';
 
-export class Payment extends BaseCustomEntity {}
+@Entity('payments')
+export class Payment extends BaseCustomEntity {
+  @Column({ nullable: false })
+  date: Date;
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({ nullable: false })
+  amount: number;
+}
