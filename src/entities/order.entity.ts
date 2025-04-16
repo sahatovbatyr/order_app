1557d6557d171b7974998b058424797883c06e44
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-16 21:20:35 +0300

    added: Order and Payment classes

---
+++ src/entities/order.entity.ts
@@ -0,0 +1,3 @@
+import { BaseCustomEntity } from '../common/ base/BaseCustomEntity';
+
+export class Order extends BaseCustomEntity {}
---
+++ src/entities/payment.entity.ts
@@ -0,0 +1,3 @@
+import { BaseCustomEntity } from '../common/ base/BaseCustomEntity';
+
+export class Payment extends BaseCustomEntity {}
