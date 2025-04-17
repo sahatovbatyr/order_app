72380b70549dc2ca4957420f896c95fdcb2ea8d7
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-18 01:23:58 +0300

    fixed:  RoleEnum initializing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { RoleService } from './modules/role/role.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly roleService: RoleService) {}
+
+  async onModuleInit() {
+    await this.roleService.seed();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
