bc023b5bbce0cca1d8bca56e83352ff480b5637f
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-18 09:12:37 +0300

    fixed: added exports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -9,5 +9,6 @@ import { User } from '../../entities/user.entity';
   imports: [TypeOrmModule.forFeature([User]), RoleModule],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
