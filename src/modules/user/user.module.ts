ae316a3eaf4c7df8b0b291eaa2a8a775407194ef
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-18 16:55:08 +0300

    feat: added AuthModule to UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { RoleModule } from '../role/role.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), RoleModule],
+  imports: [
+    forwardRef(() => AuthModule),
+    TypeOrmModule.forFeature([User]),
+    RoleModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
