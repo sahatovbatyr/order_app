913ab6e0559190bac4365c67f97e5c946616506a
sahatovbatyr <sahatovbatyr@gmail.com>
2025-04-18 09:10:41 +0300

    fixed:  added userService.seed();

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { RoleService } from './modules/role/role.service';
+import { UserService } from './modules/user/user.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(
+    private readonly roleService: RoleService,
+    private readonly userService: UserService,
+  ) {}
 
   async onModuleInit() {
     await this.roleService.seed();
+    await this.userService.seed();
   }
 
   getHello(): string {
