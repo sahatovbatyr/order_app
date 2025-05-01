7a0f7c348985e8232c1248c55df109a5cff815c7
sahatovbatyr <sahatovbatyr@gmail.com>
2025-05-01 17:49:09 +0300

    feat(role): added logger

--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -4,14 +4,16 @@ import { Role } from '../../entities/role.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
 import { RoleEnum } from '../../enums/Role.enum';
+import { CustomLoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class RoleService extends BaseCustomService<Role> {
   constructor(
     @InjectRepository(Role)
     private readonly roleRepository: Repository<Role>,
+    logger: CustomLoggerService,
   ) {
-    super(roleRepository, Role.name);
+    super(roleRepository, Role.name, logger);
   }
 
   protected getRelations(): string[] {
